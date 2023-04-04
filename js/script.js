8a2564a9f41c11da5e7d3e81c581ce25c145f374
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-04-04 16:39:23 +0000

    added site

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: colton
+// Created on: april 2023
 // This file contains the JS functions for index.html
+'use strict'
+/**
+* This function calculates area and perimeter of rectangle.
+*/
+function doMATH() {
+ // input
+ const width = parseInt(document.getElementById('Width').value)
+ const length= parseInt(document.getElementById('Length').value)
+ const height= parseInt(document.getElementById('Height').value)
+
+
+ // process
+ const area = (length * width * height) / 3
+
+
+ // output
+ document.getElementById('area').innerHTML =
+   'volume is: ' + (length * width * height) /2 * height + ' mm³'
+}
