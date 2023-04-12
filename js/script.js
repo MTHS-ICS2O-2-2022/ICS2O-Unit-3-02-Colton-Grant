573bce574f9c349b49614fd49002875b5996a86d
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-04-12 00:30:31 +0000

    added to js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // This file contains the JS functions for index.html
 'use strict'
 /**
-* This function calculates area and perimeter of rectangle.
+* This function calculates area volume of a pryamid
 */
 function doMATH() {
  // input
